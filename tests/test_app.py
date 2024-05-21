101861ca0db38e6a3d7997d20b4c5243c44d5bc8
WilliamMp <38511966+WilliamMp@users.noreply.github.com>
2024-05-20 21:36:06 -0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
 import pytest
-from flask_testing import TestCase
+# from flask_testing import TestCase
+import flask_testing 
 import sys,os
 # Adiciona o diretório raiz ao caminho do sistema
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
