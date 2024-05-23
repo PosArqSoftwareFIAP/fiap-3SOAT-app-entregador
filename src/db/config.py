30d8d0d89bf8e2ab43b930023530f57b249a886e
WilliamMp <38511966+WilliamMp@users.noreply.github.com>
2024-05-23 20:42:15 -0300

    Up

--- src/db/config.py
+++ src/db/config.py
@@ -9,3 +9,13 @@ db_password = os.getenv('MYSQL_PASSWORD')
 db_port     = 25060
 db_database = 'FIAP-FOOD'
 
+
+
+
+# # variaveis de banco de dados
+# db_host     = ${{ secrets.MYSQL_HOST }}
+# db_user     = ${{ secrets.MYSQL_PASSWORD }}
+# db_password = ${{ secrets.MYSQL_USER }}
+# db_port     = 25060
+# db_database = 'FIAP-FOOD'
+
